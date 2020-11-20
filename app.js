cc11d8c8d201d712aa662928d3d71777ab296cd8
Nicolas Rey <nicolasreydh@gmail.com>
2020-11-20 17:57:15 -0500

    Search bar design and search icon

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const burguerMenu = document.querySelector(".burguer-menu");
 const menuMobile = document.querySelector(".nav-ul");
+const searchBar = document.querySelector("#search");
+const apiKey = "DvB6aHwl4IkDw8dILVK69iWTbwQjvM5D";
+
+const searchAutocomplete = async () => {
+  try {
+    const apiCall = await fetch(
+      `https://api.giphy.com/v1/gifs/search/tags?api_key=${apiKey}&q=${searchBar.value}&limit=5`
+    );
+    const data = await apiCall.json();
+    console.log(searchBar.value);
+    console.log(data);
+  } catch (err) {
+    console.log("Hubo un error: ", err);
+  }
+};
+
+searchBar.addEventListener("input", searchAutocomplete);
 
 //Función para cambiar el ícono del burguer menú por una X
 const showHideMenu = () => {
